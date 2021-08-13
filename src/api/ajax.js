3dd9d0301cf3d4b89c8591a1e6aa07566ef8158c
liqingxin <liqingxin@migu.cn>
2021-08-13 09:23:28 +0800

    test

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,19 +1,19 @@
-import axios from "axios";
-export default function ajax(url = "", params = {}, type = "GET") {
-    let promise
-  //返回promise对象
+import axios from 'axios'
+export default function ajax(url = '', params = {}, type = 'GET') {
+  let promise
+  // 返回promise对象
   return new Promise((resolve, reject) => {
-    if ("GET" === type) {
-      promise = axios.get(url);
-    } else if ("POST" === type) {
-      promise = axios.post(url, params);
+    if (type === 'GET') {
+      promise = axios.get(url)
+    } else if (type === 'POST') {
+      promise = axios.post(url, params)
     }
     promise
-      .then((response) => {
-        resolve(response.data);
+      .then(response => {
+        resolve(response.data)
       })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+      .catch(error => {
+        reject(error)
+      })
+  })
 }
